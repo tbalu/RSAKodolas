3b32fe500ac65222d11c7356813a841146d337a4
Tóth Balázs <toth.balazs.e.pub@gmail.com>
2019-04-17 17:38:10 +0200

    Gyorshatvanyozo algoritmus.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,24 @@ import java.math.BigInteger;
 public class Main {
 
     public static void main(String [] args){
+        //System.out.println(new BigInteger(String.valueOf(12)));
         RSAKodolas rsaKodolas = new RSAKodolas();
-        BigInteger[] er = rsaKodolas.kibovitettEukledesziAlgoritmus(new BigInteger("102"),new BigInteger("101"));
+       /* BigInteger[] er = rsaKodolas.kibovitettEukledesziAlgoritmus(new BigInteger("2479"),new BigInteger("552"));
         for(BigInteger i: er){
             System.out.println(i);
-        }
+        }*/
         /*
         BigInteger a = new BigInteger("3");
         BigInteger b = new BigInteger("4");
         System.out.println(a.divide(b));*/
-    }
+
+        BigInteger m = rsaKodolas.gyorsHatvanyokzas(new BigInteger("12"),new BigInteger("34"),new BigInteger("56"));
+        System.out.println("Megoldas:");
+        System.out.println(m);
+        BigInteger b = new BigInteger("117");
+        /*
+        System.out.println(b.bitCount());
+        System.out.println(b.toString(2));
+        System.out.println(b.bitCount());
+    */}
 }
--- src/main/java/RSAKodolas.java
+++ src/main/java/RSAKodolas.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 
 
 public class RSAKodolas {
-    public BigInteger[] kibovitettEukledesziAlgoritmus(BigInteger rk,BigInteger rkp1){
+    public BigInteger[] kibovitettEukledesziAlgoritmus(BigInteger rk, BigInteger rkp1) {
        /* BigInteger r3 = new BigInteger("0");
         BigInteger egy = new BigInteger("1");
         int szamlalo=0;
@@ -22,26 +22,57 @@ public class RSAKodolas {
         BigInteger xkp1 = new BigInteger("0");
         BigInteger yk = new BigInteger("0");
         BigInteger ykp1 = new BigInteger("1");
-        System.out.println("rkp1: "+rkp1+"qkp1: "+qkp1+ "xkp1: "+xkp1+"ykp1: "+ykp1+"rk: "+rk+ "xk: "+ xk+"yk: "+yk);
-        return eukledeszSeged(rkp1,qkp1,xkp1,ykp1,rk,xk,yk);
+        System.out.println("rkp1: " + rkp1 + "qkp1: " + qkp1 + "xkp1: " + xkp1 + "ykp1: " + ykp1 + "rk: " + rk + "xk: " + xk + "yk: " + yk);
+        int szamlalo = 1;
+        return eukledeszSeged(rkp1, qkp1, xkp1, ykp1, rk, xk, yk, szamlalo);
 
     }
+
     /*private BigInteger[] eukledeszSeged(BigInteger xk, BigInteger xkp1, BigInteger yk,
                                         BigInteger ykp1, BigInteger rk, BigInteger rkp1, BigInteger qkp1){*/
     private BigInteger[] eukledeszSeged(BigInteger rkp1, BigInteger qkp1, BigInteger xkp1, BigInteger ykp1,
-                                        BigInteger rk, BigInteger xk, BigInteger yk){
-       BigInteger ru = rk.mod(rkp1);
+                                        BigInteger rk, BigInteger xk, BigInteger yk, int szamlalo) {
+        BigInteger ru = rk.mod(rkp1);
        /*BigInteger qu = rk.subtract(ru);
                     qu.divide(rkp1);*/
-       BigInteger qu = rkp1.divide(ru);
-       BigInteger xu = qkp1.multiply(xkp1).add(xk);
-       BigInteger yu = qkp1.multiply(ykp1).add(yk);
-       System.out.println("ru: "+ru+"qu: "+qu+"xu: "+xu+"yu: "+yu);
-        if(ru.equals(new BigInteger("1"))==true){
-           BigInteger[] eredmeny = {qu,xu,yu};
+        BigInteger qu = rkp1.divide(ru);
+        BigInteger xu = qkp1.multiply(xkp1).add(xk);
+        BigInteger yu = qkp1.multiply(ykp1).add(yk);
+        System.out.println("ru: " + ru + "qu: " + qu + "xu: " + xu + "yu: " + yu);
+        szamlalo++;
+        if (ru.equals(new BigInteger("1")) == true) {
+            int kitevo = (int) Math.pow(-1, szamlalo);
+            BigInteger[] eredmeny = {qu, xu.multiply(new BigInteger(String.valueOf(kitevo))),
+                    yu.multiply(new BigInteger(String.valueOf(kitevo * -1)))};
             return eredmeny;
-        }
-        else return eukledeszSeged(ru,qu,xu,yu,rkp1,xkp1,ykp1);
+        } else return eukledeszSeged(ru, qu, xu, yu, rkp1, xkp1, ykp1, szamlalo);
 
     }
+
+    public BigInteger gyorsHatvanyokzas(BigInteger alap, BigInteger kitevo, BigInteger mod) {
+        String binarisRep = kitevo.toString(2);
+        BigInteger hatvModolus = alap;
+        BigInteger modulus = BigInteger.ONE;
+        System.out.println("indulas:\n" +binarisRep);
+        while (binarisRep != null) {
+
+            alap.multiply(alap).mod(kitevo);
+            if (binarisRep.charAt(binarisRep.length() - 1) == '1') {
+                modulus = modulus.multiply(hatvModolus);
+                modulus = modulus.mod(mod);
+                System.out.println("reszeredmeny: "+ modulus);
+            }
+            if (binarisRep.length() != 1) {
+                binarisRep = binarisRep.substring(0, binarisRep.length() - 1);
+            }
+            else{
+                binarisRep = null;
+            }
+            hatvModolus =hatvModolus.pow(2).mod(mod);
+            System.out.println(binarisRep);
+            System.out.println("hatvModulus: "+ hatvModolus);
+        }
+        return modulus;
+    }
+
 }
