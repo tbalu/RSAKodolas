35800268d0a2c9c14de5c19980c8dce7171a0b6f
Tóth Balázs <toth.balazs.e.pub@gmail.com>
2019-05-11 17:35:28 +0200

    Sajat primteszt van benne. Miller-Rabin amit a Fermat teszt ellenoriz.

--- src/main/java/RSA.java
+++ src/main/java/RSA.java
@@ -14,20 +14,24 @@ public class RSA
     private int        bitlength = 1024;
     private Random     r;
 
+    public  RSA (int i){}
     public RSA()
     {
+        System.out.println("Új RSA inicializálása:");
         r = new Random();
-        p = BigInteger.probablePrime(bitlength, r);
-        q = BigInteger.probablePrime(bitlength, r);
+        /*p = BigInteger.probablePrime(bitlength, r);
+        q = BigInteger.probablePrime(bitlength, r);*/
+        p = this.veletlenPrimetGeneral();
+        System.out.println("p: " + p);
+
+        q = this.veletlenPrimetGeneral();
+        System.out.println("q: " + q);
         N = p.multiply(q);
         phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-        e = BigInteger.probablePrime(bitlength / 2, r);
+        //e = BigInteger.probablePrime(bitlength / 2, r);
+        e = this.veletlenPrimetGeneral();
+        System.out.println("e: "+ e);
         System.out.println("e: "+e);
-        /*while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
-        {
-            e.add(BigInteger.ONE);
-        }
-        d = e.modInverse(phi);*/
         d = kibovitettEukledesziAlgoritmus(phi,e);
         String testString = "a";
         byte[] titkositott = titkosit(testString.getBytes());
@@ -41,16 +45,9 @@ public class RSA
             System.out.println("helytelen");
             d = phi.subtract(d);
         }
-        //BigInteger d2 = phi.subtract(d);
+
     }
-    public BigInteger fun(BigInteger r,BigInteger phi){
-        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
-        {
-            e.add(BigInteger.ONE);
-        }
-        d = e.modInverse(phi);
-        return d;
-    }
+
     public RSA(BigInteger e, BigInteger d, BigInteger N)
     {
         this.e = e;
@@ -58,11 +55,32 @@ public class RSA
         this.N = N;
     }
 
-    @SuppressWarnings("deprecation")
+
     public static void main(String[] args) throws IOException {
+        /*BigInteger b = new BigInteger("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+        System.out.println(b.intValue());
+        System.out.println(b.mod(new BigInteger("2")));
 
+        RSA rsa = new RSA(12);
+        System.out.println("Miller : " + rsa.MillerRabinTeszt(new BigInteger("703")) + "fermat: " + rsa.Fermatteszt(new BigInteger("703")));
+        BigInteger bigIntegeri = new BigInteger("2");
+        for(int i = 0 ; i < 1001; i++){
 
-        while (true) {
+        bigIntegeri = bigIntegeri.add(BigInteger.ONE);
+        ;
+        if(bigIntegeri.mod(new BigInteger("2")).equals(BigInteger.ONE) && rsa.MillerRabinTeszt(bigIntegeri))
+            System.out.println(bigIntegeri + " prim? " );
+        //System.out.println( + " prim? ");
+
+        }
+        for(int i = 0; i < 10; i++){
+            System.out.println(rsa.veletlenPrimetGeneral());
+        }*/
+        /*
+        RSA rsa = new RSA(2);
+        System.out.println(rsa.Fermatteszt(new BigInteger("70")));
+        System.out.println(rsa.veletlenPrimetGeneral());*/
+       while (true) {
             RSA rsa = new RSA();
 
 
@@ -74,10 +92,18 @@ public class RSA
             System.out.println("A szöveg bájtokban: "
                     + bytesToString(teststring.getBytes()));
             // titkosit
+            System.out.println("Titkosit");
             byte[] titkositott = rsa.titkosit(teststring.getBytes());
+            System.out.println("Titkosított bajtsorozat");
+            for(byte ba: titkositott){
+                System.out.print(ba);
+            }
+            System.out.println("\n");
+            System.out.println("Feltor");
             // feltor
             byte[] feltort = rsa.feltor(titkositott);
-            System.out.println("Feltöröm ezeket a bájtokat: " + bytesToString(feltort));
+
+            System.out.println("Feltört bájtok: " + bytesToString(feltort));
             System.out.println("Feltört szöveg: " + new String(feltort));
         }
     }
@@ -94,10 +120,8 @@ public class RSA
     // Encrypt message
     public byte[] titkosit(byte[] message)
     {
-        System.out.println("Titkosít");
-        System.out.println("---------------------------------------------------------------------");
-        //System.out.println("alap: " +(new BigInteger(message))+"\nkitevo: "+ e+"\nmodulus: "+N);
-       // return (new BigInteger(message)).modPow(e, N).toByteArray();
+
+        System.out.println("alap: " +(new BigInteger(message))+"\nkitevo: "+ e+"\nmodulus: "+N);
         return gyorsHatvanyokzas((new BigInteger(message)),e,N).toByteArray();
 
     }
@@ -105,10 +129,7 @@ public class RSA
     // Feltori az uzenetet
     public byte[] feltor(byte[] message)
     {
-        System.out.println("Feltör");
-        System.out.println("---------------------------------------------------------------------");
-        //System.out.println("alap: " +(new BigInteger(message))+"\nkitevo: "+ d+"\nmodulus: "+N);
-        //return (new BigInteger(message)).modPow(d, N).toByteArray();
+
         return gyorsHatvanyokzas((new BigInteger(message)),d,N).toByteArray();
     }
 
@@ -120,7 +141,6 @@ public class RSA
         BigInteger xkp1 = new BigInteger("0");
         BigInteger yk = new BigInteger("0");
         BigInteger ykp1 = new BigInteger("1");
-        //System.out.println("rkp1: " + rkp1 + "qkp1: " + qkp1 + "xkp1: " + xkp1 + "ykp1: " + ykp1 + "rk: " + rk + "xk: " + xk + "yk: " + yk);
         int szamlalo = 1;
         return eukledeszSeged(rkp1, qkp1, xkp1, ykp1, rk, xk, yk, szamlalo,rkp1);
 
@@ -133,14 +153,12 @@ public class RSA
         BigInteger qu = rkp1.divide(ru);
         BigInteger xu = qkp1.multiply(xkp1).add(xk);
         BigInteger yu = qkp1.multiply(ykp1).add(yk);
-        //System.out.println("ru: " + ru + "qu: " + qu + "xu: " + xu + "yu: " + yu);
         szamlalo++;
         if (ru.equals(new BigInteger("1")) == true) {
             int kitevo = (int) Math.pow(-1, szamlalo);
             BigInteger[] eredmeny = {qu, xu.multiply(new BigInteger(String.valueOf(kitevo))),
                     yu.multiply(new BigInteger(String.valueOf(kitevo * -1)))};
-            //return yu.multiply(new BigInteger(String.valueOf(kitevo * -1)));
-            //return yu;
+
             if(yu.compareTo(BigInteger.ZERO)==1){
                 return yu;
             }
@@ -150,16 +168,12 @@ public class RSA
     }
 
     public BigInteger gyorsHatvanyokzas(BigInteger alap, BigInteger kitevo, BigInteger mod) {
-        //System.out.println("alap: "+ alap);
-        //System.out.println("kitevo: "+ kitevo);
-       // System.out.println("mod: "+ mod);
         String binarisRep = kitevo.toString(2);
         BigInteger hatvModolus = alap;
         BigInteger modulus = BigInteger.ONE;
-        //System.out.println("indulas:\n" +binarisRep);
+
         while (binarisRep != null) {
 
-//            alap.multiply(alap).mod(kitevo);
             if (binarisRep.charAt(binarisRep.length() - 1) == '1') {
                 modulus = modulus.multiply(hatvModolus);
                 modulus = modulus.mod(mod);
@@ -172,10 +186,66 @@ public class RSA
                 binarisRep = null;
             }
             hatvModolus =hatvModolus.pow(2).mod(mod);
-           /* System.out.println(binarisRep);
-            System.out.println("hatvModulus: "+ hatvModolus);*/
+
         }
         return modulus;
+    }
+
+    public BigInteger veletlenPrimetGeneral(){
+        r = new Random();
+        BigInteger primJelolt = new BigInteger(bitlength/2,r);
+        boolean primetTalaltam=false;
+        while(  !primetTalaltam ){
+            if(MillerRabinTeszt(primJelolt)){
+                if (Fermatteszt(primJelolt)) {
+                    return primJelolt;
+                }
+            }
+            primJelolt = new BigInteger(bitlength/2,r);
+
+        }
+        System.out.println("primet talaltam");
+        return primJelolt;
+    }
+    private boolean Fermatteszt(BigInteger p){
+        BigInteger alap = new BigInteger(p.bitCount(),new Random());
+        alap = alap.subtract(BigInteger.ONE);
+       //System.out.println(alap);
+        if(alap.modPow(p.subtract(BigInteger.ONE) , p).compareTo(BigInteger.ONE)==0 ){
+            return true;
+        }
+        return false;
+    }
+    public boolean MillerRabinTeszt(BigInteger primJelolt){
+        //BigInteger alap = new BigInteger("11");
+        BigInteger alap = new BigInteger(primJelolt.bitCount(),new Random());
+        alap = alap.subtract(BigInteger.ONE);
+        BigInteger pm1 = primJelolt.subtract(BigInteger.ONE);
+        BigInteger ketto = new BigInteger("2");
+        BigInteger s = new BigInteger("0");
+        while(pm1.divide(ketto).mod(ketto)==BigInteger.ZERO){
+            pm1 = pm1.divide(ketto);
+                s =  s.add(BigInteger.ONE);
+               // System.out.println(s);
+
+        }
+        //System.out.println("s ertek: " + s);
+
+        for (; s.compareTo(BigInteger.ZERO)!=-1; s =  s.subtract(BigInteger.ONE)){
+                /*System.out.println("forban vagyok");
+                System.out.println(s);
+                System.out.println(ketto.pow(s.intValue()));
+                System.out.println("alap : " + alap);
+                System.out.println("kitevo:" + ketto.pow(s.intValue()).multiply(pm1));
+                System.out.println("primjelolt: "  + primJelolt);
+                System.out.println(alap.modPow(ketto.pow(s.intValue()).multiply(pm1),primJelolt));*/
+            //System.out.println(alap.modPow(ketto.pow(s.intValue()).multiply(pm1),primJelolt));
+                if(alap.modPow(ketto.pow(s.intValue()).multiply(pm1),primJelolt).compareTo(BigInteger.ONE)==0){
+               // System.out.println("Prim");
+                return true;
+            }
+        }
+        return false;
     }
 
 }
